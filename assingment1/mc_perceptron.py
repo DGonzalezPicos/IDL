eaf4b7d7f7c4023c3c548ab83cd723e7b4ddf0d2
Lukas Welzel <lukas.leonard.welzel@gmail.com>
2022-09-28 16:58:58 +0200

    T2 working but dumb
    maybe its just guessing, who knows, its consistently 2% better than random guessing :D

--- assingment1/mc_perceptron.py
+++ assingment1/mc_perceptron.py
@@ -1,4 +1,5 @@
 import numpy as np
+from tqdm import tqdm
 
 class MultiClassPerceptron(object):
     def __init__(self,
@@ -9,9 +10,9 @@ class MultiClassPerceptron(object):
 
         ### DEFAULT VALUES
         default_settings = {
-            "learning_rate": 1e-5,
-            "epochs": 1000,
-            "weight_scale": 1e-4
+            "learning_rate": 1e-2,
+            "epochs": 100,
+            "weight_scale": 1e-2
         }
         if isinstance(settings, dict):
             self.settings = {**default_settings, **settings}
@@ -26,6 +27,7 @@ class MultiClassPerceptron(object):
         # TODO: randomly initialize via rng
         self.weights = self.rng.uniform(-1, 1, size=(257, 10)) * self.settings["weight_scale"]
         self.bias = np.zeros(shape=(257, 10))
+        self.__truth = np.zeros(10)
         self.activation_func = self.__argmax  # self.__step_func
 
 
@@ -34,31 +36,49 @@ class MultiClassPerceptron(object):
 
         _X = self.__append_bias(X)
 
-        # TODO: this is the current sharpening, either 1 or 0
-        _y = np.copy(y)
-        _y[y > 0.] = 1
-        _y[y <= 0.] = 0
+        for __ in tqdm(np.arange(self.epochs)):
+            for x, y_train in zip(_X, y):
+                self.__truth[y_train] = 1.
 
-        for __ in np.arange(self.epochs):
-            for x, y_train in zip(_X, _y):
-                y_predicted = self.predict(x)
-                error = y_train - y_predicted
+                y_predicted = self.__predict(x)
+                error = self.__truth - y_predicted
 
                 update = self.learning_rate * error
-                self.weights += update * x
+
+                self.weights += np.outer(x, update) * self.__sigmoid_prime(np.dot(x, self.weights))
                 self.bias += update
 
+                self.__truth[y_train] = 0.
+
+
+
+    def __predict(self, x):
+        lin_out = np.dot(x, self.weights)  # X @ self.weights.T + self.bias
+        y_predicted = self.__sigmoid(lin_out)
+        return y_predicted
 
     def predict(self, X):
-        lin_out = X @ self.weights.T + self.bias
-        y_predicted = self.activation_func(lin_out)
-        return y_predicted
+        _X = self.__append_bias(X)
+        predictions = np.zeros(len(_X))
+        for i, x in enumerate(_X):
+            y = self.__predict(x)
+            predictions[i] = np.argmax(y)
+
+        return predictions
+
 
     def __step_func(self, x):
         return np.where(x >= 0, 1, 0)
 
     def __argmax(self, x):
         return np.argmax(x)
+
+    def __sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def __sigmoid_prime(self, x):
+        x = self.__sigmoid(x)
+        return x * (1. - x)
 
     @staticmethod
     def __append_bias(X):
@@ -69,6 +89,33 @@ class MultiClassPerceptron(object):
     def accuracy(y_true, y_pred=None):
         if y_pred is not None:
             return np.mean(y_true == y_pred)
+        else:
+            raise NotImplementedError
+
+def train_mc_perceptron():
+    import matplotlib
+    # matplotlib.use('TkAgg')
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    df_in = pd.read_csv('train_in.csv', header=None)
+    train_in = df_in.to_numpy()
+
+    df_out = pd.read_csv('train_out.csv', header=None)
+    train_out = df_out.to_numpy()
+
+    percy = MultiClassPerceptron()
+    percy.fit(train_in, train_out)
+
+    df_in = pd.read_csv('test_in.csv', header=None)
+    test_in = df_in.to_numpy()
+
+    df_out = pd.read_csv('test_out.csv', header=None)
+    test_out = df_out.to_numpy()
+
+    predictions = percy.predict(test_in)
+
+    print(f"Accuracy: {percy.accuracy(test_out, predictions)}")
+
 
 if __name__ == '__main__':
-    pass
+    train_mc_perceptron()
